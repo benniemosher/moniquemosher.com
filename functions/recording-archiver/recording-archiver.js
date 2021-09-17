65316e21be7478d8256b93639b733067de85c4b3
Bennie Mosher <benniemosher@gmail.com>
2021-09-17 10:02:52 -0600

    Adding console.log of body

--- functions/recording-archiver/recording-archiver.js
+++ functions/recording-archiver/recording-archiver.js
@@ -2,7 +2,13 @@
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
-    console.log(event)
+    // console.log(event)
+
+    // TODO: Change to env variable
+    // if (event.headers.authorization == "MvAe8ECXQBCW1eICf0QKDw") {
+      console.log(event.body)
+    // }
+
     return {
       statusCode: 200,
       body: JSON.stringify(event),
