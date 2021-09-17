4b8a2585d4e3cdb511f540788a38efd90e237a2a
Bennie Mosher <benniemosher@gmail.com>
2021-09-17 10:12:28 -0600

    Adding output for download files

--- functions/recording-archiver/recording-archiver.js
+++ functions/recording-archiver/recording-archiver.js
@@ -5,16 +5,22 @@ const handler = async (event) => {
     // console.log(event)
 
     // TODO: Change to env variable
-    // if (event.headers.authorization == "MvAe8ECXQBCW1eICf0QKDw") {
+    if (event.headers.authorization == "MvAe8ECXQBCW1eICf0QKDw") {
       console.log(event.body)
-    // }
+      let payload = event.body.payload
+      let title = payload.topic
+      let download_files = []
+
+      payload.recording_files.forEach((file) => {
+        download_files.push(file.download_url)
+      })
+
+      console.log(`download files: ${JSON.stringify(download_files)}`)
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(event),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({'message':'hello world'})
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
